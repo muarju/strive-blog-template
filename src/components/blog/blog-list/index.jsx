b105b847628341b0e78c98a0db2a3f1f94f9cce3
Engr. Md. Muslim Uddin Arju <mdarju240@gmail.com>
2021-08-26 22:47:35 +0100

    Dynamiclly showing post

--- src/components/blog/blog-list/index.jsx
+++ src/components/blog/blog-list/index.jsx
@@ -1,14 +1,28 @@
 import React, { Component } from "react";
 import { Row, Col } from "react-bootstrap";
 import BlogItem from "../blog-item";
-import posts from "../../../data/posts.json";
+//import posts from "../../../data/posts.json";
+
+
 export default class BlogList extends Component {
+  state = {
+    posts: []
+  }
+
+  componentDidMount = async () =>{
+    let res = await fetch(process.env.REACT_APP_PROD_API_URL + '/blogPost')
+    let blogPosts = await res.json()
+    console.log(blogPosts)
+    this.setState({
+        posts: [...blogPosts]
+    })
+}
   render() {
     return (
       <Row>
-        {posts.map((post) => (
+        {this.state.posts.map((post) => (
           <Col md={4} style={{ marginBottom: 50 }}>
-            <BlogItem key={post.title} {...post} />
+            <BlogItem key={post.id} {...post} />
           </Col>
         ))}
       </Row>
